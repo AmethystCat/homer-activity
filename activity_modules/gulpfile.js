2432990e354e4a20920a00798bec7c6a1444de09
HC <lilan1990917@gmail.com>
2016-10-08 11:49:03 +0800

    rename

---
+++ activity_modules/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	sourcemap = require('gulp-sourcemaps'),
+	plumber = require('gulp-plumber'),
+	bs = require('browser-sync').create();
+
+var paths = {
+		less: 'src/less/*.less',
+		css: 'build/css/*.css',
+		scripts: 'build/js/*.js',
+		html: 'build/*.html'
+	};
+
+gulp.task('bs', function() {
+	bs.init({
+		server: {
+			baseDir: './build'
+		}
+	});
+
+	gulp.watch(paths.less, ['less']);
+	gulp.watch(paths.css).on('change', function(e) {
+		bs.reload(e.path);
+	});
+	gulp.watch(paths.scripts).on('change', bs.reload);
+	gulp.watch(paths.html).on('change', bs.reload);
+});
+gulp.task('less', function() {
+	return gulp.src(paths.less)
+		.pipe(plumber())
+		.pipe(sourcemap.init())
+		.pipe(less())
+		.pipe(sourcemap.write())
+		.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('imgmin', function() {});
+
+gulp.task('build', function() {});
+
+gulp.task('default', ['less', 'bs']);
