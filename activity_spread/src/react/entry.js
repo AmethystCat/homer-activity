756e182571f5660a72e56fe423dde808dc760723
HC <lilan1990917@gmail.com>
2016-10-08 18:23:06 +0800

    add env console

--- activity_spread/src/react/entry.js
+++ activity_spread/src/react/entry.js
@@ -2,7 +2,7 @@ import	React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Test from './pages/login_regist.jsx';
-
+console.log(process.env.NODE_ENV);
 ReactDOM.render(
 	<h1>Index</h1>,
 	document.getElementById('app')
