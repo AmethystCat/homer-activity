7c080f85751466712a39674d6390792f165810a4
HC <lilan1990917@gmail.com>
2016-10-13 03:36:14 +0800

    add saler

---
+++ activity_spread/src/react/pages/saler/add.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+class Add extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>添加销售员</div>
+        );
+    }
+}
